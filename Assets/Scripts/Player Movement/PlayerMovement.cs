98fa4e6ff11a30ed7fc1a52359b2b2930d6c7b9c
Roy Jared <17jroy@cr.k12.ia.us>
2017-04-21 08:39:45 -0500

    texture and movement
    Added a test texture to the floor, and finished up basic movement.

--- Assets/Scripts/Player Movement/PlayerMovement.cs
+++ Assets/Scripts/Player Movement/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+	public float moveSpeed = 10.0f;
+	public float rotateSpeed = 5.0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,21 +15,32 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey("w")) {
-			transform.Translate (Vector3.forward);
-			Debug.Log ("Pressing W key");
+			transform.Translate ((Vector3.forward) * moveSpeed * Time.deltaTime);
 		}
-		if (Input.GetButtonDown("Testing")) {
-			Debug.Log (Input.GetAxis ("Testing"));
-			Debug.Log ("Pressing Testing key");
+		if (Input.GetKey("s")) {
+			transform.Translate ((Vector3.back) * moveSpeed * Time.deltaTime);
 		}
 		if (Input.GetKey("a")) {
-			Debug.Log ("Pressing A key");
+			transform.Rotate ((Vector3.down) * rotateSpeed);
+			//transform.Translate ((Vector3.left) * moveSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey("d")) {
+			transform.Rotate ((Vector3.up) * rotateSpeed);
+			//transform.Translate ((Vector3.right) * moveSpeed * Time.deltaTime);
 		}
 		if(Input.GetMouseButton(1)){
+			moveSpeed = 20.0f;
 			Debug.Log ("Mouse Button Right");
 		}
+		if (!Input.GetMouseButton (1)) {
+			moveSpeed = 10.0f;
+		}
 		if(Input.GetMouseButton(0)){
+			moveSpeed = 5.0f;
 			Debug.Log ("Mouse Button left");
+		}
+		if (!Input.GetMouseButton (0)) {
+			moveSpeed = 10.0f;
 		}
 	}
 }
